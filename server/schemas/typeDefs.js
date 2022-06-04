f7c8b99f3e0df014b10d997ae80becb721cab54c
Spencer Hulse <hulse.spencer@gmail.com>
2022-06-04 00:56:21 -0400

    Fixed errors preventing server start.

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,5 +1,17 @@
-const gql = require("apollo-server-express");
+const { gql } = require("apollo-server-express");
 
-const typeDefs = gql``;
+const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    email: String
+    admin: Boolean
+  }
+
+  type Query {
+    users: [User]
+    user: User
+  }
+`;
 
 module.exports = typeDefs;
--- server/server.js
+++ server/server.js
@@ -5,12 +5,17 @@ const path = require("path");
 const { ApolloServer } = require("apollo-server-express");
 const { typeDefs, resolvers } = require("./schemas");
 const { authMiddleware } = require("./utils/auth");
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context: authMiddleware,
-});
-server.applyMiddleware({ app });
+const startServer = async () => {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: authMiddleware,
+  });
+  await server.start();
+  server.applyMiddleware({ app });
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+};
+startServer();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -20,6 +25,5 @@ const PORT = process.env.PORT || 3001;
 db.once("open", () => {
   app.listen(PORT, () => {
     console.log(`API server is listening on port ${PORT}...`);
-    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
   });
 });
