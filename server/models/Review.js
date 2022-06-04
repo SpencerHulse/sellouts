60948f278198967aecfbe09f66a98d395feac96f
Spencer Hulse <hulse.spencer@gmail.com>
2022-06-04 02:34:32 -0400

    Added the Review model.

--- server/models/Review.js
+++ server/models/Review.js
@@ -1,7 +1,37 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
-const reviewSchema = new Schema({});
+const reviewSchema = new Schema({
+  review: {
+    type: String,
+    required: true,
+    min: 1,
+    max: 255,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  score: {
+    type: Number,
+    required: true,
+    min: 1,
+    max: 5,
+  },
+  upvotes: {
+    type: Number,
+    default: 0,
+  },
+  downvotes: {
+    type: Number,
+    default: 0,
+  },
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+});
 
 const Review = mongoose.model("Review", reviewSchema);
 
