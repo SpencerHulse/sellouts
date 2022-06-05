33a379f8fefb2341d5208c960ce9a344d5c04ad4
Spencer Hulse <hulse.spencer@gmail.com>
2022-06-05 01:58:23 -0400

    Added User mutations, including login.

--- client/src/graphql/mutations.js
+++ client/src/graphql/mutations.js
@@ -27,4 +27,52 @@ export const UPDATE_CATEGORY = gql`
   }
 `;
 
+export const ADD_USER = gql`
+  mutation AddUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+        admin
+      }
+    }
+  }
+`;
+
+export const DELETE_USER = gql`
+  mutation DeleteUser($id: ID!) {
+    deleteUser(_id: $id) {
+      _id
+      email
+    }
+  }
+`;
+
+export const UPDATE_USER = gql`
+  mutation UpdateUser($username: String, $email: String, $password: String) {
+    updateUser(username: $username, email: $email, password: $password) {
+      _id
+      username
+      email
+      admin
+    }
+  }
+`;
+
+export const LOGIN = gql`
+  mutation Login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+        admin
+      }
+    }
+  }
+`;
+
 export const PLACEHOLDER = gql``;
--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,3 +1,5 @@
+const { AuthenticationError } = require("apollo-server-express");
+const { signToken } = require("../utils/auth");
 const {
   Category,
   Order,
@@ -25,15 +27,66 @@ const resolvers = {
   },
   Mutation: {
     // Category Mutations
-    addCategory: (parent, args) => {
+    addCategory: async (parent, args) => {
       return Category.create(args);
     },
-    deleteCategory: (parent, { _id }) => {
+    deleteCategory: async (parent, { _id }) => {
       // .exec() prevents "Query was already executed" error
       return Category.findByIdAndDelete({ _id }).exec();
     },
-    updateCategory: (parent, args) => {
+    updateCategory: async (parent, args) => {
       return Category.findByIdAndUpdate(args._id, args, { new: true }).exec();
+    },
+    addUser: async (parent, args) => {
+      let usernameCheck = await User.find({ username: args.username });
+      if (usernameCheck.length) {
+        console.log(usernameCheck);
+        return { message: "Username already exists" };
+      }
+
+      let emailCheck = await User.find({ email: args.email });
+      if (emailCheck.length) {
+        return { message: "There is already an account with this email" };
+      }
+
+      let passwordCheck = args.password.length;
+      if (passwordCheck < 5) {
+        return { message: "The password must be at least 5 characters" };
+      }
+
+      const user = await User.create(args);
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    deleteUser: async (parent, { _id }) => {
+      return User.findByIdAndDelete({ _id });
+    },
+    updateUser: async (parent, args, context) => {
+      if (context.user) {
+        return await User.findByIdAndUpdate(context.user._id, args, {
+          new: true,
+        });
+      }
+
+      throw new AuthenticationError("Not logged in");
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError("Incorrect credentials");
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError("Incorrect credentials");
+      }
+
+      const token = signToken(user);
+
+      return { token, user };
     },
   },
 };
