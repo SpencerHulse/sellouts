6697383be38027c23871159e116e7b7ec300732e
Spencer Hulse <hulse.spencer@gmail.com>
2022-06-25 19:22:16 -0400

    Updated the success page to match the styling of other pages.

--- client/src/pages/purchase-success.js
+++ client/src/pages/purchase-success.js
@@ -19,19 +19,19 @@ function PurchaseSuccess() {
   useProducts();
 
   return (
-    <div className="mt-5">
-      <div className="success-container shadow">
-        <div className="success-header d-flex align-items-center justify-content-center">
-          <div className="logo">Sellouts</div>
-        </div>
-        <h2 className="mt-4 text-center">Thank you for your purchase!</h2>
-        <p className="mt-3 mb-5 text-center">
-          The order was created successfully.
-        </p>
-        <div>
-          <p className="mt-2 pb-4 text-center">
-            <a href="/">Continue shopping</a>
+    <div className="bg-tint fullpage row m-0">
+      <div className="text-center col p-0">
+        <h1 className="m-5">Success!</h1>
+        <div className="bg-white pt-5 pb-5 account-mid">
+          <h2 className="mt-4 text-center">Thank you for your purchase!</h2>
+          <p className="mt-3 mb-5 text-center">
+            The order was processed successfully.
           </p>
+          <div>
+            <p className="mt-2 pb-4 text-center">
+              <a href="/">Click here</a> to continue shopping
+            </p>
+          </div>
         </div>
       </div>
     </div>
