3df4df40bd13fa13592ecba2636df596d245d9bd
Spencer Hulse <hulse.spencer@gmail.com>
2022-06-28 02:23:32 -0400

    Added production code.

--- server/server.js
+++ server/server.js
@@ -20,6 +20,17 @@ startServer();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Serve up static assets
+app.use("/images", express.static(path.join(__dirname, "../client/images")));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+}
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
+
 const db = require("./config/connection");
 const PORT = process.env.PORT || 3001;
 db.once("open", () => {
