94eff7517145f7af9d11b946150d4708b09fbe4a
Spencer Hulse <hulse.spencer@gmail.com>
2022-06-30 17:31:34 -0400

    Added toasts for when items are added to cart on homepage and single-product page.

--- client/src/components/Products/ProductCard/index.js
+++ client/src/components/Products/ProductCard/index.js
@@ -1,5 +1,6 @@
-import React from "react";
+import { useState } from "react";
 import ReactStars from "react-stars";
+import { ToastContainer, ToastHeader, Toast } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { addToCart } from "../../../redux/features/cartSlice";
@@ -23,6 +24,7 @@ function ProductCard({ product }) {
 
   const dispatch = useDispatch();
   const { cartItems } = useSelector((state) => state.cart);
+  const [show, setShow] = useState(false);
 
   function addItemToCart(quantity) {
     const itemInCart = cartItems.find((item) => item._id === _id);
@@ -44,67 +46,79 @@ function ProductCard({ product }) {
   }
 
   return (
-    <div className="pc-container d-flex flex-sm-nowrap flex-md-wrap">
-      <Link to={`/product/${_id}`} className="me-md-3 me-xl-0 mb-2 box">
-        <img
-          className="pc-image"
-          src={mainImage}
-          alt={`${name} ${category} ${mainImage}`}
-        />
-      </Link>
-      <div className="pc-details ms-3 ms-md-0">
-        <Link to={`/product/${_id}`} className="pc-title">
-          {name}
+    <>
+      <ToastContainer style={{ position: "fixed", top: 0, right: 0 }}>
+        <Toast onClose={() => setShow(false)} show={show} delay={2000} autohide>
+          <ToastHeader className="justify-content-between me-2">
+            <Toast.Body>{name} added to cart!</Toast.Body>
+          </ToastHeader>
+        </Toast>
+      </ToastContainer>
+      <div className="pc-container d-flex flex-sm-nowrap flex-md-wrap">
+        <Link to={`/product/${_id}`} className="me-md-3 me-xl-0 mb-2 box">
+          <img
+            className="pc-image"
+            src={mainImage}
+            alt={`${name} ${category} ${mainImage}`}
+          />
         </Link>
-        <div>
-          <div className="pc-priceblock">
-            {effectivePromotion(promotion, ends) === false ? (
-              <div className="pc-price font-weight-bold bolden">${price}</div>
-            ) : (
-              <>
-                <div className="d-flex">
-                  <div className="pc-price text-decoration-line-through me-2">
-                    ${price}
-                  </div>
-                  <div className="pc-price pc-promoprice">
-                    <div className="d-flex">
-                      <div>
-                        <span className="bolden me-1">${promotionPrice}</span>
+        <div className="pc-details ms-3 ms-md-0">
+          <Link to={`/product/${_id}`} className="pc-title">
+            {name}
+          </Link>
+          <div>
+            <div className="pc-priceblock">
+              {effectivePromotion(promotion, ends) === false ? (
+                <div className="pc-price font-weight-bold bolden">${price}</div>
+              ) : (
+                <>
+                  <div className="d-flex">
+                    <div className="pc-price text-decoration-line-through me-2">
+                      ${price}
+                    </div>
+                    <div className="pc-price pc-promoprice">
+                      <div className="d-flex">
+                        <div>
+                          <span className="bolden me-1">${promotionPrice}</span>
+                        </div>
+                        <div>({percentage}% off)</div>
                       </div>
-                      <div>({percentage}% off)</div>
                     </div>
                   </div>
-                </div>
-              </>
-            )}
-          </div>
-          <div>
-            <ReactStars
-              count={5}
-              value={rating}
-              size={18}
-              edit={false}
-              color2={"#7f60db"}
-              color1={"#e3e3e3"}
-            />
-          </div>
-          <div>
-            {inventory <= 0 ? (
-              <button className="addtocart-unavailable mt-2" disabled>
-                Unavailable
-              </button>
-            ) : (
-              <button
-                className="addtocart mt-2"
-                onClick={() => addItemToCart(1)}
-              >
-                Add to Cart
-              </button>
-            )}
+                </>
+              )}
+            </div>
+            <div>
+              <ReactStars
+                count={5}
+                value={rating}
+                size={18}
+                edit={false}
+                color2={"#7f60db"}
+                color1={"#e3e3e3"}
+              />
+            </div>
+            <div>
+              {inventory <= 0 ? (
+                <button className="addtocart-unavailable mt-2" disabled>
+                  Unavailable
+                </button>
+              ) : (
+                <button
+                  className="addtocart mt-2"
+                  onClick={() => {
+                    addItemToCart(1);
+                    setShow(true);
+                  }}
+                >
+                  Add to Cart
+                </button>
+              )}
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
--- client/src/pages/single-product.js
+++ client/src/pages/single-product.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import { useLazyQuery } from "@apollo/client";
 import { useParams } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
+import { ToastContainer, ToastHeader, Toast } from "react-bootstrap";
 import ReactStars from "react-stars";
 import ReviewList from "../components/ReviewList";
 import ReviewForm from "../components/Reviews";
@@ -16,7 +17,8 @@ function SingleProduct() {
   const dispatch = useDispatch();
   const { products } = useSelector((state) => state.products);
   const { cartItems } = useSelector((state) => state.cart);
-
+  // Toast state
+  const [show, setShow] = useState(false);
   // Holds the current product object
   const [currentProduct, setCurrentProduct] = useState("");
   // Gets the product ID
@@ -78,117 +80,134 @@ function SingleProduct() {
   return (
     <>
       {currentProduct ? (
-        <div>
-          <div className="bg-tint mb-4">
-            <div className="container">
-              <div className="row gx-4 fullpage align-items-stretch">
-                <div className="col-xl-8 col-md-12 my-auto">
-                  <img
-                    src={currentProduct.mainImage}
-                    alt={currentProduct.name}
-                    className="product-image"
-                  />
-                </div>
-                <div className="product-details p-4 h-100 col-xl-4 col-md-12 my-auto text-center">
-                  <h1 className="fw-light mb-4">{currentProduct.name}</h1>
-                  <div className="pc-priceblock mb-2">
-                    {effectivePromotion(currentProduct.promotion, ends) ===
+        <>
+          <ToastContainer style={{ position: "fixed", top: 0, right: 0 }}>
+            <Toast
+              onClose={() => setShow(false)}
+              show={show}
+              delay={2000}
+              autohide
+            >
+              <ToastHeader className="justify-content-between me-2">
+                <Toast.Body>{currentProduct.name} added to cart!</Toast.Body>
+              </ToastHeader>
+            </Toast>
+          </ToastContainer>
+          <div>
+            <div className="bg-tint mb-4">
+              <div className="container">
+                <div className="row gx-4 fullpage align-items-stretch">
+                  <div className="col-xl-8 col-md-12 my-auto">
+                    <img
+                      src={currentProduct.mainImage}
+                      alt={currentProduct.name}
+                      className="product-image"
+                    />
+                  </div>
+                  <div className="product-details p-4 h-100 col-xl-4 col-md-12 my-auto text-center">
+                    <h1 className="fw-light mb-4">{currentProduct.name}</h1>
+                    <div className="pc-priceblock mb-2">
+                      {effectivePromotion(currentProduct.promotion, ends) ===
                       false ? (
-                      <div className="pc-price font-weight-bold bolden">
-                        ${currentProduct.price}
-                      </div>
-                    ) : (
-                      <div className="d-xs-column d-md-flex justify-content-center">
-                        <div className="pc-price text-decoration-line-through me-2">
+                        <div className="pc-price font-weight-bold bolden">
                           ${currentProduct.price}
                         </div>
-                        <div className="pc-price pc-promoprice">
-                          <div className="d-sm-column d-md-flex">
-                            <div>
-                              <span className="bolden me-1">
-                                ${currentProduct.promotionPrice}
-                              </span>
+                      ) : (
+                        <div className="d-xs-column d-md-flex justify-content-center">
+                          <div className="pc-price text-decoration-line-through me-2">
+                            ${currentProduct.price}
+                          </div>
+                          <div className="pc-price pc-promoprice">
+                            <div className="d-sm-column d-md-flex">
+                              <div>
+                                <span className="bolden me-1">
+                                  ${currentProduct.promotionPrice}
+                                </span>
+                              </div>
+                              <div>({percentage}% off)</div>
                             </div>
-                            <div>({percentage}% off)</div>
                           </div>
                         </div>
+                      )}
+                    </div>
+                    <div className="d-flex justify-content-center mb-3">
+                      <ReactStars
+                        count={5}
+                        value={currentProduct.rating}
+                        size={18}
+                        edit={false}
+                        color2={"#7f60db"}
+                        color1={"rgba(0, 0, 0, 0.19)"}
+                      />
+                      <div className="review-button ms-2">
+                        ({currentProduct.reviews.length} reviews)
                       </div>
-                    )}
-                  </div>
-                  <div className="d-flex justify-content-center mb-3">
-                    <ReactStars
-                      count={5}
-                      value={currentProduct.rating}
-                      size={18}
-                      edit={false}
-                      color2={"#7f60db"}
-                      color1={"rgba(0, 0, 0, 0.19)"}
-                    />
-                    <div className="review-button ms-2">
-                      ({currentProduct.reviews.length} reviews)
+                    </div>
+                    <div className="product-description lh-lg">
+                      <p className="mx-1">
+                        {shortDescription(currentProduct.description)}
+                        {currentProduct.description.length > 50 && (
+                          <>
+                            ... <a href="#description-anchor">Read more</a>.
+                          </>
+                        )}
+                      </p>
+                    </div>
+                    <div>
+                      <button
+                        className="addtocart mt-2"
+                        onClick={() => {
+                          addItemToCart(1);
+                          setShow(true);
+                        }}
+                      >
+                        Add to Cart
+                      </button>
                     </div>
                   </div>
-                  <div className="product-description lh-lg">
-                    <p className="mx-1">
-                      {shortDescription(currentProduct.description)}
-                      {currentProduct.description.length > 50 && (
-                        <>
-                          ... <a href="#description-anchor">Read more</a>.
-                        </>
-                      )}
-                    </p>
-                  </div>
-                  <div>
-                    <button
-                      className="addtocart mt-2"
-                      onClick={() => addItemToCart(1)}
-                    >
-                      Add to Cart
-                    </button>
-                  </div>
                 </div>
               </div>
             </div>
-          </div>
-          <div className="bg-tint pt-5 pb-5 mb-4">
-            <div className="container" id="description-anchor">
-              <div className="row">
-                <div className="col-12 col-md-6">
-                  {" "}
-                  <h2 className="fw-light mb-4 h1">Description</h2>
-                  {currentProduct.description}
+            <div className="bg-tint pt-5 pb-5 mb-4">
+              <div className="container" id="description-anchor">
+                <div className="row">
+                  <div className="col-12 col-md-6">
+                    {" "}
+                    <h2 className="fw-light mb-4 h1">Description</h2>
+                    {currentProduct.description}
+                  </div>
+                  <ul className="col-md-3 product-details mt-5 mt-md-0">
+                    <h2 className="text-start fw-light mb-4 h1">Details</h2>
+                    {currentProduct.details.length ? (
+                      currentProduct.details.map((detail, index) => {
+                        return (
+                          <li key={`${productId}-detail-${index}`}>{detail}</li>
+                        );
+                      })
+                    ) : (
+                      <li>Nothing here yet...</li>
+                    )}
+                  </ul>
                 </div>
-                <ul className="col-md-3 product-details mt-5 mt-md-0">
-                  <h2 className="text-start fw-light mb-4 h1">Details</h2>
-                  {currentProduct.details.length ? (
-                    currentProduct.details.map((detail, index) => {
-                      return (
-                        <li key={`${productId}-detail-${index}`}>{detail}</li>
-                      );
-                    })
-                  ) : (
-                    <li>Nothing here yet...</li>
-                  )}
-                </ul>
+              </div>
+            </div>
+            <div className="bg-tint p-5 text-center">
+              <div className="container">
+                <h2 className="fw-light mb-3 h1">Reviews</h2>
+                {currentProduct.reviews.length ? (
+                  <ReviewList currentProduct={currentProduct} />
+                ) : (
+                  <div className="mb-3">
+                    There are no reviews yet. Be the first to leave one!
+                  </div>
+                )}
+                {Auth.loggedIn() && (
+                  <ReviewForm currentProduct={currentProduct} />
+                )}
               </div>
             </div>
           </div>
-          <div className="bg-tint p-5 text-center">
-            <div className="container">
-              <h2 className="fw-light mb-3 h1">Reviews</h2>
-              {currentProduct.reviews.length ? (
-                <ReviewList currentProduct={currentProduct} />
-              ) : (
-                <div className="mb-3">
-                  There are no reviews yet. Be the first to leave one!
-                </div>
-              )}
-              {Auth.loggedIn() && (
-                <ReviewForm currentProduct={currentProduct} />
-              )}
-            </div>
-          </div>
-        </div>
+        </>
       ) : (
         <h2>Loading...</h2>
       )}
